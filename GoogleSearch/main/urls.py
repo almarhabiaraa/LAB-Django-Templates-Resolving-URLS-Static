f6bbb2734fc417704fbc3ca2b66c1b26c04085a7
almarhabiaraa <almarhabiaraa@gmail.com>
2026-04-06 04:25:37 +0300

    update terms page and search functionality

--- GoogleSearch/main/urls.py
+++ GoogleSearch/main/urls.py
@@ -7,5 +7,4 @@ app_name = "main"
 urlpatterns = [
     path('', views.home_view, name="home"),
     path('terms/', views.terms_view, name="terms"),
-     path('search/', views.search, name='search'),
 ]
